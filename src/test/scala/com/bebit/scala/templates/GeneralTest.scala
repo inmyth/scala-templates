80cb67a3077b03909a858c39453eab04f1f1d96e
Martin <martin.bcu@gmail.com>
2019-12-20 16:51:11 +0900

    Added mocked example in WorkflowTest.scala

--- src/test/scala/com/bebit/scala/templates/GeneralTest.scala
+++ src/test/scala/com/bebit/scala/templates/GeneralTest.scala
@@ -1,21 +1,22 @@
 package com.bebit.scala.templates
 
+import com.github.tototoshi.csv.CSVReader
 import monix.eval.Task
+import org.scalamock.scalatest.AsyncMockFactory
 import org.scalatest.flatspec.AsyncFlatSpec
 
 import scala.collection.mutable
 
-class GeneralTest extends AsyncFlatSpec{
+class GeneralTest extends AsyncFlatSpec with AsyncMockFactory{
   import monix.execution.Scheduler.Implicits.global
 
   def addSoon(addends: Int*): Task[Int] = Task { addends.sum }
 
   behavior of "addSoon"
 
+  // this is a concurrent test
   it should "eventually compute a sum of passed Ints" in {
     val futureSum: Task[Int] = addSoon(1, 2)
-    // You can map assertions onto a Future, then return
-    // the resulting Future[Assertion] to ScalaTest:
     val z = futureSum.runToFuture
     z map { sum => assert(sum == 3) }
   }
@@ -34,4 +35,10 @@ class GeneralTest extends AsyncFlatSpec{
       emptyStack.pop()
     }
   }
+
+
+
+
+
+
 }
--- src/test/scala/com/bebit/scala/templates/WorkflowTest.scala
+++ src/test/scala/com/bebit/scala/templates/WorkflowTest.scala
@@ -1,8 +1,10 @@
 package com.bebit.scala.templates
 
+import com.github.tototoshi.csv.CSVReader
+import org.scalamock.scalatest.AsyncMockFactory
 import org.scalatest.flatspec.AsyncFlatSpec
 
-class WorkflowTest extends AsyncFlatSpec{
+class WorkflowTest extends AsyncFlatSpec with AsyncMockFactory{
   import Workflow._
 
   behavior of "parseCSV"
@@ -12,5 +14,19 @@ class WorkflowTest extends AsyncFlatSpec{
       List("user_id", "123456789", "visit_id", "0")
     )
     assert(parseCSV(x).size == 1)
+  }
+
+  "parseCSV" should "parse from CSVReader" in {
+    val mockedCSVReader = mock[CSVReader]
+
+    // Set expectations
+    (mockedCSVReader.all _).expects().returning(
+      List (
+        List("user_id", "123456789", "visit_id", "0"),
+        List("user_id", "666666666", "visit_id", "0")
+      )
+    )
+
+    assert(mockedCSVReader.all().map(toDeleteKey).size == 2)
   }
 }
