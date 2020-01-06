731e79d16c8e0ac0154532144cca8e2cc3ca99a1
inmyth <ad3a5a3adc570d224e6509a39bee52cc0d36b55b>
2020-01-06 18:04:04 +0900

    Updated CassandraDatastaxBasic.scala

--- src/main/scala/com/bebit/scala/templates/connection/CassandraDatastaxBasic.scala
+++ src/main/scala/com/bebit/scala/templates/connection/CassandraDatastaxBasic.scala
@@ -3,28 +3,25 @@ package com.bebit.scala.templates.connection
 import com.bebit.scala.templates.connection.cassandra.CassandraHelper
 import com.datastax.oss.driver.api.core.CqlSession
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
 object CassandraDatastaxBasic extends App {
   import CassandraHelper._
-
   import monix.execution.Scheduler.Implicits.global
 
   implicit val session = CqlSession
     .builder()
     .build()
 
-  val compose = execute(cql"select release_version from system.local").runToFuture
+  sys.addShutdownHook(session.close())
 
-  compose
+  val compose = execute(cql"select release_version from system.local")
+
+  compose.runToFuture
     .map(p => {
       val result = p.one().getString("release_version")
       println(result)
     })
     .recover{case e => println(e)}
 
-//  Await.ready(compose, Duration.Inf)
 }
 
 
