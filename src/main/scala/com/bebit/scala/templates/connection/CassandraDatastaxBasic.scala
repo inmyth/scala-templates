cc9b99654ecd166f46cec8d4f2df5d1756452a85
inmyth <ad3a5a3adc570d224e6509a39bee52cc0d36b55b>
2020-01-06 15:08:02 +0900

    Changed CassandraHelper from Future to Task

--- src/main/scala/com/bebit/scala/templates/connection/CassandraDatastaxBasic.scala
+++ src/main/scala/com/bebit/scala/templates/connection/CassandraDatastaxBasic.scala
@@ -3,22 +3,28 @@ package com.bebit.scala.templates.connection
 import com.bebit.scala.templates.connection.cassandra.CassandraHelper
 import com.datastax.oss.driver.api.core.CqlSession
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 object CassandraDatastaxBasic extends App {
   import CassandraHelper._
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  import monix.execution.Scheduler.Implicits.global
 
   implicit val session = CqlSession
     .builder()
     .build()
 
-  execute(cql"select release_version from system.local")
+  val compose = execute(cql"select release_version from system.local").runToFuture
+
+  compose
     .map(p => {
       val result = p.one().getString("release_version")
       println(result)
     })
     .recover{case e => println(e)}
 
+  Await.ready(compose, Duration.Inf)
 }
 
 
--- src/main/scala/com/bebit/scala/templates/connection/cassandra/CassandraHelper.scala
+++ src/main/scala/com/bebit/scala/templates/connection/cassandra/CassandraHelper.scala
@@ -2,22 +2,22 @@ package com.bebit.scala.templates.connection.cassandra
 
 import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.cql.{AsyncResultSet, PreparedStatement, SimpleStatement}
+import monix.eval.Task
 
-import scala.concurrent.{ExecutionContext, Future}
 
 object CassandraHelper {
 
   implicit class CqlStrings(val context: StringContext) extends AnyVal {
 
-    def cql(args: Any*)(implicit session: CqlSession): Future[PreparedStatement] = {
+    def cql(args: Any*)(implicit session: CqlSession): Task[PreparedStatement] = {
       val statement =  SimpleStatement.newInstance(context.raw(args: _*))
-      scala.compat.java8.FutureConverters.toScala(session.prepareAsync(statement))
+      Task.fromFuture(scala.compat.java8.FutureConverters.toScala(session.prepareAsync(statement)))
     }
   }
 
-  def execute(statement: Future[PreparedStatement], params: Any*)(implicit executionContext: ExecutionContext, session: CqlSession): Future[AsyncResultSet] =
+  def execute(statement: Task[PreparedStatement], params: Any*)( implicit session: CqlSession): Task[AsyncResultSet] =
     statement
       .map(p => if(params.nonEmpty) p.bind(params.map(_.asInstanceOf[Object]) ) else p.bind())
-      .map(session.executeAsync(_))
-      .flatMap(scala.compat.java8.FutureConverters.toScala)
+      .flatMap(p => Task.fromFuture(scala.compat.java8.FutureConverters.toScala(session.executeAsync(p)))
+    )
 }
